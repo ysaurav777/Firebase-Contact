1f777e25cc7bd5f4e55074a846005f72f888b803
Saurav Yadav <ysaurav777@gmail.com>
2024-04-08 23:01:20 +0530

    new changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import '../Styles/Home.scss';
+import Contact from '../Assests/Hands_Contact.png';
+import { IoSearchSharp } from "react-icons/io5";
+import { GoPlus } from "react-icons/go";
 
 const Home = () => {
   return (
     <main className='home-main'>
       <div>
         <div className='search-div'>
+          <div>
+            <IoSearchSharp className='icons-search'/>
+          </div>
           <div className='search-input'>
             <input className='input-area' placeholder='Search Contact'/>
           </div>
           <div className='add-contact'>
-            <h1>+</h1>
+            <GoPlus className='plus-sym'/>
           </div>
         </div>
       </div>
+      <div className='home-contact'>
+          <div className='home1-contact'>
+            <div>
+              <img src={Contact} alt="No Contact"/>
+            </div>
+            <div>
+              <h1 className='no-contact'>No Contact Found</h1>
+            </div>
+          </div>
+        </div>
     </main>
   )
 }
